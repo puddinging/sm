b6ae0afb104e60816622db100ac2bb86e2546f5e
jiefeng.wang <jiefeng.wang@outlook.com>
2023-11-12 21:05:21 +0800

    feature(): 暂时去掉自动补全功能

--- cmd/open.go
+++ cmd/open.go
@@ -6,7 +6,6 @@ package cmd
 import (
 	"errors"
 	"sm/utils"
-	"strings"
 
 	"github.com/spf13/cobra"
 )
@@ -18,7 +17,6 @@ var (
 func init() {
 	rootCmd.AddCommand(openCmd)
 	openCmd.Flags().StringVarP(&alias, "alias", "a", "", "alias")
-	openCmd.Flags().SetAnnotation("alias", cobra.BashCompCustom, []string{"__sm_alias"})
 }
 
 // openCmd represents the open command
@@ -35,19 +33,4 @@ var openCmd = &cobra.Command{
 			return errors.New("no matching server found")
 		}
 	},
-}
-
-func __sm_alias(cmd *cobra.Command, args []string, toComplete string) ([]string, cobra.ShellCompDirective) {
-	// 在这里实现自动补全的逻辑，可以根据你的服务器列表或配置文件提供建议
-	suggestions := []string{"apaas-demo", "apaas-dev", "apaas-qa", "apaas-prod", "apaas-new-stable", "apaas-dev2"}
-	filtered := make([]string, 0)
-
-	// 过滤匹配的建议
-	for _, s := range suggestions {
-		if strings.HasPrefix(s, toComplete) {
-			filtered = append(filtered, s)
-		}
-	}
-
-	return filtered, cobra.ShellCompDirectiveNoFileComp
 }
