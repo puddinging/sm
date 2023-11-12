032afd68afb503fdcea181b6619a768991521044
jiefeng.wang <jiefeng.wang@outlook.com>
2023-11-12 20:13:09 +0800

    feature(): 实现掉add命令

--- README.md
+++ README.md
@@ -25,9 +25,13 @@ Alias: local      IP: 0.0.0.0
 Alias: local1     IP: 192.168.0.1    
 --------------
 ```
-3. `open` 命令，使用-a指定 list 命令列出的 `Alias` 即可。可在当前命令行打开 `ssh` 链接
+2. `open` 命令，使用-a指定 list 命令列出的 `Alias` 即可。可在当前命令行打开 `ssh` 链接
 ```
 sm open -a local
+```
+3. `add` 命令，在配置文件里边添加一个新的服务器连接信息
+```
+sm add -a local -i 192.168.0.1 -u jiefeng -p welcome1
 ```
 ## 注意
 1. `Linux/macos` 需安装 `sshpass` 工具
--- cmd/add.go
+++ cmd/add.go
@@ -4,10 +4,14 @@ Copyright © 2023 NAME HERE <EMAIL ADDRESS>
 package cmd
 
 import (
+	"encoding/json"
 	"errors"
 	"fmt"
 	"sm/model"
 	"sm/utils"
+
+	"io/fs"
+	"os"
 
 	"github.com/spf13/cobra"
 )
@@ -29,13 +33,8 @@ func init() {
 // addCmd represents the add command
 var addCmd = &cobra.Command{
 	Use:   "add",
-	Short: "A brief description of your command",
-	Long: `A longer description that spans multiple lines and likely contains examples
-and usage of using your command. For example:
-
-Cobra is a CLI library for Go that empowers applications.
-This application is a tool to generate the needed files
-to quickly create a Cobra application.`,
+	Short: "add a server infomation",
+	Long:  ``,
 	// DisableFlagParsing: true,
 	RunE: func(cmd *cobra.Command, args []string) error {
 		fmt.Printf("add commond ip: %s, username: %s, password: %s.\n", ip, username, password)
@@ -51,6 +50,22 @@ to quickly create a Cobra application.`,
 		}
 		serverList := config.ServerList
 		serverList = append(serverList, server)
+		config.ServerList = serverList
+		WriteConfig(serverList, configPath)
 		return nil
 	},
+}
+
+func WriteConfig(config interface{}, filePath string) error {
+	configJSON, err := json.MarshalIndent(config, "", "  ")
+	if err != nil {
+		return errors.New("无法序列化配置为 JSON 格式")
+	}
+
+	err = os.WriteFile(filePath, configJSON, fs.FileMode(0644))
+	if err != nil {
+		return errors.New("无法写入配置文件")
+	}
+
+	return nil
 }
