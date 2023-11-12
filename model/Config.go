10aea10e76e2b980c829dc32b333d13e80f0a80b
jiefeng.wang <jiefeng.wang@outlook.com>
2023-11-12 19:58:52 +0800

    feature(): 添加保活策略

--- model/Config.go
+++ model/Config.go
@@ -2,9 +2,13 @@ package model
 
 import (
 	"fmt"
+	"io"
 	"os"
 	"os/exec"
 	"runtime"
+	"time"
+
+	"golang.org/x/term"
 )
 
 type Server struct {
@@ -28,23 +32,71 @@ func (s *Server) Connect() error {
 			"-P", "22", // 指定SSH端口
 			"-pw", s.Password,
 			"-batch", // 禁用交互模式，确保不会出现任何提示
+			"-o", "TCPKeepAlive=yes",
 		)
 	} else {
 		// 如果不是Windows系统，直接使用sshpass和ssh命令
 		cmd = exec.Command("sshpass",
 			"-p", s.Password,
 			"ssh",
+			"-t", // 强制分配伪终端
 			fmt.Sprintf("%s@%s", s.Username, s.IP),
 			"-o", "StrictHostKeyChecking=no",
 			"-o", "UserKnownHostsFile=/dev/null",
+			"-o", "TCPKeepAlive=yes",
 		)
 	}
 
 	cmd.Stdout = os.Stdout
 	cmd.Stderr = os.Stderr
-	cmd.Stdin = os.Stdin
 
-	err := cmd.Run()
+	var stdinPipe io.WriteCloser
+	var err error
+
+	if term.IsTerminal(int(os.Stdin.Fd())) {
+		// 如果stdin是终端，强制分配伪终端
+		cmd.Stdin = os.Stdin
+	} else {
+		// 如果stdin不是终端，创建管道并使用它
+		stdinPipe, err = cmd.StdinPipe()
+		if err != nil {
+			return err
+		}
+		defer stdinPipe.Close()
+
+		go func() {
+			// 从os.Stdin复制到stdinPipe
+			io.Copy(stdinPipe, os.Stdin)
+		}()
+	}
+
+	err = cmd.Start()
+	if err != nil {
+		return err
+	}
+
+	// 启动保活goroutine
+	go func() {
+		ticker := time.NewTicker(1 * time.Minute) // 每分钟发送一次保活请求
+		defer ticker.Stop()
+
+		for range ticker.C {
+			if cmd.ProcessState != nil && cmd.ProcessState.Exited() {
+				break
+			}
+
+			if stdinPipe != nil {
+				// 如果stdinPipe存在，使用它发送保活信号
+				_, err := stdinPipe.Write([]byte("\n"))
+				if err != nil {
+					fmt.Println("Error writing to stdin:", err)
+					break
+				}
+			}
+		}
+	}()
+
+	err = cmd.Wait()
 	if err != nil {
 		return err
 	}
